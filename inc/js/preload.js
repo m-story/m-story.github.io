8200647b45d26ae5d02633f373d4d1268e0134d7
m-story <69177852+m-story@users.noreply.github.com>
2020-09-08 10:03:36 +0900

    Add files via upload

---
+++ inc/js/preload.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    soul_preload();
+
+    function soul_preload() {
+
+        preloadImage  = new Image();
+
+        for(var i=0; i<soul.length; i++){
+            var img1 = soul_obj[soul[i]].img1;
+            var img2 = soul_obj[soul[i]].img2;
+
+            for(var p=0; p<img1.length; p++){
+                preloadImage.src="data/img/soul/"+img1[p];
+            }
+            
+            for(var q=0; q<img2.length; q++){
+                preloadImage.src="data/img/soul/"+img2[p];
+            }
+    
+        }
+        
+    }
+
+});
