949cc37c7f650880bab9353a56864ae41f1d6133
m-story <69177852+m-story@users.noreply.github.com>
2020-09-08 10:12:36 +0900

    Update preload.js

--- inc/js/preload.js
+++ inc/js/preload.js
@@ -3,18 +3,18 @@ $(document).ready(function(){
     soul_preload();
 
     function soul_preload() {
-
-        preloadImage  = new Image();
-
+        
         for(var i=0; i<soul.length; i++){
             var img1 = soul_obj[soul[i]].img1;
             var img2 = soul_obj[soul[i]].img2;
 
             for(var p=0; p<img1.length; p++){
+                preloadImage  = new Image();
                 preloadImage.src="data/img/soul/"+img1[p];
             }
             
             for(var q=0; q<img2.length; q++){
+                preloadImage  = new Image();
                 preloadImage.src="data/img/soul/"+img2[p];
             }
     
